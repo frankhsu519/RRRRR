7d7bcf7e40fc0377a665d207c50fa6003ece8061
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2022-01-26 13:45:24 +0800

    移除console.log

--- split_second_stage.js
+++ split_second_stage.js
@@ -61,7 +61,7 @@ function transe_credit(accordingTheseData){
   creditor.forEach(credit=>{
     creditor_list.push(credit.net_worth)
   })
-  console.log('債務人',debtor,'債權人', creditor);
+  // console.log('債務人',debtor,'債權人', creditor);
   render_best_split(debtor, creditor)
 
 }
@@ -157,6 +157,7 @@ function get_sec_user_arr(){
 function render_best_split(debtor, creditor){
   var debt_temp = ''
   var credit_temp = ''
+  debugger
   // console.log('債務人：', debtor,'債權人:' ,creditor);
   $('#best_split_wrapper').html(`<h3 id='pay_someone_block2_title'>最佳化分帳</h3><ul id="pay_someone_block_best_split" class="rounded"></ul>`)
   debtor.forEach(item=>{
